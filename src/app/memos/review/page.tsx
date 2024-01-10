1877645226089e8531a159e7b36ea3f725bfce4a
Will Holmes <willholmes97@icloud.com>
2024-01-10 16:58:47 +0000

    Markdown renderer added

--- src/app/memos/review/page.tsx
+++ src/app/memos/review/page.tsx
@@ -1,7 +1,11 @@
-export default function ReviewMemoPage() {
+import ReactMarkdown from "react-markdown";
+
+export default async function ReviewMemoPage() {
+  const contentHtml = "# Hello World!";
   return (
     <div>
       <h1>Review Memo</h1>
+      <ReactMarkdown>{contentHtml}</ReactMarkdown>
     </div>
   );
 }
