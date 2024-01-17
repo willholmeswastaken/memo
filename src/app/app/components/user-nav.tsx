1f2a059d1eef166733e86dff42a51c04bd964e49
Will Holmes <willholmes97@icloud.com>
2024-01-17 17:55:12 +0000

    pull in users details to dropdown

--- src/app/app/components/user-nav.tsx
+++ src/app/app/components/user-nav.tsx
@@ -10,25 +10,35 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  LogoutLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function UserNav() {
+export async function UserNav() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>WH</AvatarFallback>
+            <AvatarFallback>
+              {user?.given_name?.charAt(0)}
+              {user?.family_name?.charAt(0)}
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">devwillholmes</p>
+            <p className="text-sm font-medium leading-none">
+              {user?.given_name}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
-              info@willholmes.dev
+              {user?.email}
             </p>
           </div>
         </DropdownMenuLabel>
